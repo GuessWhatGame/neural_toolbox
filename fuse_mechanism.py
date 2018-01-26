1e1fefdba3ecfbebd47563ef500fff7a03d2f641
Florian Strub <florian.strub@gmail.com>
2018-01-26 17:29:54 +0100

    Add multilayer, fuse_mechanism, remove old utils

---
+++ fuse_mechanism.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+import tensorflow.contrib.layers as tfc_layers
+
+
+def fuse_by_concat(input1, input2):
+    with tf.variable_scope('concat'):
+        return tf.concat([input1, input2], axis=-1)
+
+
+def fuse_by_dot_product(input1, input2):
+    with tf.variable_scope('dot'):
+        assert input1.get_shape()[1] == input2.get_shape()[1]
+        return input1 * input2
+
+
+def fuse_by_brut_force(input1, input2):
+    with tf.variable_scope('brut_force_fusion'):
+        assert tf.size(input1) == tf.size(input2) and tf.size(input1) == 2
+
+        tf.concat([input1, input2, tf.abs(input1 - input2), input1 * input2], axis=-1)
+
+
+def fuse_by_vis(input1, input2, projection_size, output_size, dropout_keep, activation=tf.nn.tanh, reuse=False):
+    with tf.variable_scope('vis_fusion'):
+
+        input1_projection = tfc_layers.fully_connected(input1,
+                                                       num_outputs=projection_size,
+                                                       activation_fn=activation,
+                                                       reuse=reuse,
+                                                       scope="input1_projection")
+
+        input2_projection = tfc_layers.fully_connected(input2,
+                                                       num_outputs=projection_size,
+                                                       activation_fn=activation,
+                                                       reuse=reuse,
+                                                       scope="input2_projection")
+
+        full_projection = input1_projection * input2_projection
+        full_projection = tf.nn.dropout(full_projection, dropout_keep)
+
+        output = tfc_layers.fully_connected(full_projection,
+                                            num_outputs=output_size,
+                                            activation_fn=activation,
+                                            reuse=reuse,
+                                            scope="final_projection")
+
+        return output
--- utils.py
+++ utils.py
@@ -3,44 +3,36 @@ from tensorflow.python.ops.init_ops import UniformUnitScaling, Constant
 
 #TODO slowly delete those modules
 
-def fully_connected(inp, n_out, activation=None, scope="fully_connected",
-                    weight_initializer=UniformUnitScaling(),
-                    init_bias=0.0, use_bias=True, reuse=False):
-    with tf.variable_scope(scope, reuse=reuse):
-        inp_size = int(inp.get_shape()[1])
-        shape = [inp_size, n_out]
-        weight = tf.get_variable(
-            "W", shape,
-            initializer=weight_initializer)
-        out = tf.matmul(inp, weight)
 
-        if use_bias:
-            bias = tf.get_variable(
-                "b", [n_out],
-                initializer=Constant(init_bias))
-            out += bias
+from tensorflow.python.layers import base
 
-    if activation == 'relu':
-        return tf.nn.relu(out)
-    if activation == 'softmax':
-        return tf.nn.softmax(out)
-    if activation == 'tanh':
-        return tf.tanh(out)
-    return out
+class MultiLayers(base.Layer):
+   """ Multi Layer Class - Applies multiple layers sequentially """
+
+   def __init__(self, layers, **kwargs):
+       """ Constructor
+           :param layers: A list of layers to apply
+           :param kwargs: Optional. Keyword arguments
+       """
+       super(MultiLayers, self).__init__(**kwargs)
+       self.layers = layers
+
+   def call(self, inputs, **kwargs):
+       """ Sequentially calls each layer with the output of the previous one """
+       outputs = inputs
+       for layer in self.layers:
+           outputs = layer(outputs)
+       return outputs
+
+   def _compute_output_shape(self, input_shape):
+       """ Computes the output shape given the input shape """
+       output_shape = input_shape
+       for layer in self.layers:
+           output_shape = layer._compute_output_shape(output_shape)
+       return output_shape
 
 
-def rank(inp):
-    return len(inp.get_shape())
 
-
-def cross_entropy(y_hat, y):
-    if rank(y) == 2:
-        return -tf.reduce_mean(y * tf.log(y_hat))
-    if rank(y) == 1:
-        ind = tf.range(tf.shape(y_hat)[0]) * tf.shape(y_hat)[1] + y
-        flat_prob = tf.reshape(y_hat, [-1])
-        return -tf.log(tf.gather(flat_prob, ind))
-    raise ValueError('Rank of target vector must be 1 or 2')
 
 
 def masked_softmax(scores, mask):
